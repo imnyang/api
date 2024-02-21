fa088a51385ded09ed0c168b68ba1a31c08512f0
imnyang <me@imnyang.xyz>
2024-02-21 20:54:22 +0900

    API의 가독성 해결

--- func.py
+++ func.py
@@ -9,10 +9,12 @@ def now(where:str):
         "success": True,
         "timestamp": time,
         "all": time.strftime('%Y-%m-%d %H:%M:%S'),
-        "year": time.strftime('%Y'),
-        "month": time.strftime('%m'),
-        "day": time.strftime('%d'),
-        "hour": time.strftime('%H'),
-        "minute": time.strftime('%M'),
-        "second": time.strftime('%S'),
+        "time": {
+            "year": time.strftime('%Y'),
+            "month": time.strftime('%m'),
+            "day": time.strftime('%d'),
+            "hour": time.strftime('%H'),
+            "minute": time.strftime('%M'),
+            "second": time.strftime('%S'),
+        },
     }
